a2a948b229c04de5e177f1864172d5db77b5dc17
Roman Pekarev <92672092+ropek745@users.noreply.github.com>
2022-09-11 15:02:50 +0300

    Update and rename backend/foodgram/api/urls.py to серверная часть/пищевая программа/API/urls.py

--- серверная часть/пищевая программа/API/urls.py
+++ серверная часть/пищевая программа/API/urls.py
@@ -9,6 +9,8 @@ router_v1 = DefaultRouter()
 router_v1.register('tags', TagViewSet, basename='tags')
 router_v1.register('users', UsersViewSet, basename='users')
 router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
+router_v1.register('recipes', RecipeViewSet, basename='recipes')
+
 
 urlpatterns = [
     path('', include(router_v1.urls)),
