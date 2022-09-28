146aa7fbd694de8080f4c234f0e8351ded5f46f5
Roman Pekarev <92672092+ropek745@users.noreply.github.com>
2022-09-28 15:48:39 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = os.getenv('DEBUG', default=False)
 ALLOWED_HOSTS = ['*']
 
 
